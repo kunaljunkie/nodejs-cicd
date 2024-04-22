beaa85303b02ad14858001926f289bbb48c8189c
Kunal Kumawat <kunalkumawat7860@gmail.com>
2024-04-22 12:20:50 +0530

    initial

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 8080
 const app = express()
 
 app.get('/',(req,res)=>{
-    console.log(req)
+    // console.log(req)
     res.status(200).json({status:200,message:"nothing to show this side: author: 'kunal kumawat'"})
 })
 
